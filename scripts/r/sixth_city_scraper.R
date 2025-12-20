9e58356bade5ddd889f1e650a7f712e59c8dda11
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-12-20 09:42:22 -0500

    style(R): de-lint 6c scraper. Add stg_ before table outputs.

--- scripts/r/sixth_city_scraper.R
+++ scripts/r/sixth_city_scraper.R
@@ -5,43 +5,42 @@ path_json <- "data/sixth-city/content/"
 
 # Initialize data tables
 tables <- list(
-  'draft' = c('draft_id', 'type', 'season', 'round', 'pick'),
-  'franchises' = c('franchise_id', 'season'),
-  'league' = c('league_id'),
-  'schedule' = c('week', 'franchise_id', 'season'),
-  'standings' = c('franchise_id', 'franchise_name', 'season'),
-  'starters' = c('franchise_id', 'franchise_name', 'week', 'season'),
-  'transactions' = c('timestamp'),
+  "draft" = c("draft_id", "type", "season", "round", "pick"),
+  "franchises" = c("franchise_id", "season"),
+  "league" = c("league_id"),
+  "schedule" = c("week", "franchise_id", "season"),
+  "standings" = c("franchise_id", "franchise_name", "season"),
+  "starters" = c("franchise_id", "franchise_name", "week", "season"),
+  "transactions" = c("timestamp"),
   # below tables are full, not incremental, load
-  'draftpicks' = c(),
-  'rosters' = c()
-  )
-
+  "draftpicks" = c(),
+  "rosters" = c()
+)
 
 # Read the JSON file
-league_configs <- fromJSON(paste0(path_json, 'league_config.json'))
+league_configs <- fromJSON(paste0(path_json, "league_config.json"))
 
 # Iterate with a for loop
-for (i in 1:nrow(league_configs)) {
+for (i in seq_len(nrow(league_configs))) {
 
   # Connect to the league
   con <- ff_connect(
     platform = league_configs$platform[i],
     league_id = league_configs$league_id[i],
     season = league_configs$season[i]
-    )
-  
-  for (t in 1:length(tables)) {
+  )
+
+  for (t in seq_along(tables)) {
     table <- tables[t]
-    temp_path <- paste0(path_api, names(table), '.csv')
-    temp_data <- get(paste0("ff_", names(table)))(con) %>% 
+    temp_path <- paste0(path_api, "stg_", names(table), ".csv")
+    temp_data <- get(paste0("ff_", names(table)))(con) %>%
       mutate(season = league_configs$season[i])
-    if (names(table) %in% c('draftpicks', 'rosters')) {
-      write.csv(temp_data, temp_path, row.names = F)
+    if (names(table) %in% c("draftpicks", "rosters")) {
+      write.csv(temp_data, temp_path, row.names = FALSE)
       cat(paste0("Rewritten ", temp_path, " (", nrow(temp_data), " rows)\n"))
     } else {
       upsert(
-        temp_data, temp_path, key_cols = unname(unlist(table)), write = T
+        temp_data, temp_path, key_cols = unname(unlist(table)), write = TRUE
       )
     }
   }
