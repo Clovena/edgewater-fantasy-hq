a4b42b80e7b761124ae6b61142d597d3f6c2b394
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-12-20 23:21:02 -0500

    style(R): de-lint utils.

--- scripts/r/utils.R
+++ scripts/r/utils.R
@@ -3,19 +3,19 @@ library(jsonlite)
 library(ffscrapr)
 library(tidyverse)
 
-upsert <- function(new_data, file_path, key_cols, write = F) {
-  new_data <- new_data %>% 
-    mutate(across(everything(), as.character))
+upsert <- function(new_data, file_path, key_cols, write = FALSE) {
+  new_data <- new_data %>%
+    dplyr::mutate(dplyr::across(tidyselect::everything(), as.character))
   if (file.exists(file_path)) {
-    existing <- read.csv(file_path) %>% 
-      mutate(across(everything(), as.character))
-    kept <- anti_join(existing, new_data, by = key_cols)
-    final <- bind_rows(kept, new_data)
+    existing <- read.csv(file_path) %>%
+      dplyr::mutate(dplyr::across(tidyselect::everything(), as.character))
+    kept <- dplyr::anti_join(existing, new_data, by = key_cols)
+    final <- dplyr::bind_rows(kept, new_data)
   } else {
     final <- new_data
   }
   if (write) {
-    write.csv(final, file_path, row.names = F)
+    write.csv(final, file_path, row.names = FALSE)
     cat(paste0("Updated ", file_path, " (", nrow(new_data), " rows)\n"))
   } else {
     final
