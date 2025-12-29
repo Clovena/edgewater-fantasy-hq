bccf5cbeee6533ffee81854f70f46509b5f090f3
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-12-29 10:15:36 -0500

    build(scripts): aggregator for 6c fact models.

---
+++ scripts/r/sixth_city_models.R
@@ -0,0 +1,4 @@
+source("scripts/r/utils.R")
+source("scripts/r/models/sixth-city/fact_draft.R")
+source("scripts/r/models/sixth-city/fact_franchises.R")
+source("scripts/r/models/sixth-city/fact_schedule.R")
