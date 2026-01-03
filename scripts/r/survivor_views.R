0737595590491048de99aea74ae8532ef698168a
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2026-01-03 18:25:04 -0500

    feat(svr): correct v fact standings table.

--- scripts/r/survivor_views.R
+++ scripts/r/survivor_views.R
@@ -13,18 +13,34 @@ team_metadata <- do.call(
 
 stg_franchises <- read.csv("data/survivor/api/stg_franchises.csv")
 stg_starters <- read.csv("data/survivor/api/stg_starters.csv")
+stg_schedule <- read.csv("data/survivor/api/stg_schedule.csv") %>%
+  select(season, week, franchise_id, franchise_score)
 
 surviving <- stg_starters %>%
   select(season, week, franchise_id) %>%
   distinct() %>%
+  select(-week) %>%
   group_by(season, franchise_id) %>%
-  summarise(weeks_alive = n()) %>%
+  mutate(weeks_alive = n()) %>%
+  ungroup() %>%
+  distinct() %>%
+  left_join(stg_schedule,
+    by = c(
+      "season" = "season",
+      "franchise_id" = "franchise_id",
+      "weeks_alive" = "week"
+    )
+  ) %>%
+  rowwise() %>%
+  mutate(tiebreak = franchise_score / 5000) %>%
+  group_by(season) %>%
+  mutate(weeks_alive = row_number(weeks_alive + tiebreak)) %>%
   left_join(
     stg_franchises %>%
       select(season, franchise_id, user_id),
     join_by(season, franchise_id)
   ) %>%
-  select(-franchise_id) %>%
+  select(-c(franchise_id, franchise_score, tiebreak)) %>%
   left_join(team_metadata, join_by(user_id)) %>%
   arrange(season, -weeks_alive) %>%
   relocate(season, user_id, owner_name, abbrev, weeks_alive)
